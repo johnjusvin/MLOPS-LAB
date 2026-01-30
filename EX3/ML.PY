3bfc1400e63d29382c954c892a9b59a18e219261
Students <Students@WS24AC01168.karunya.edu>
2026-01-30 12:06:22 +0530

    added ex3

---
+++ EX3/ML.PY
@@ -0,0 +1,18 @@
+from sklearn.datasets import load_iris
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LogisticRegression
+from sklearn.metrics import accuracy_score
+# Load dataset
+X, y = load_iris(return_X_y=True)
+# Split data
+X_train, X_test, y_train, y_test = train_test_split(
+X, y, test_size=0.2, random_state=42
+)
+# Train model
+model = LogisticRegression(max_iter=200)
+model.fit(X_train, y_train)
+# Predict
+y_pred = model.predict(X_test)
+# Accuracy
+accuracy = accuracy_score(y_test, y_pred)
+print("Model Accuracy:", accuracy)
